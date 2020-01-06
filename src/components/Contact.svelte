6266dcf75b81cfd2785db1a0e799df8a7b163b1c
leonkalema <kaliguy7@gmail.com>
2020-01-06 22:20:09 +0100

    initial commit

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,5 +1,6 @@
 <script>
 import { onMount } from 'svelte';
+import DonationWidget from './DonationWidget.svelte';
 
 onMount( () => {
 
@@ -11,7 +12,7 @@ onMount( () => {
            <div class="flex flex-wrap h-full">
                <div class="contact--item-map bg-center bg-cover w-full hidden lg:block lg:w-1/2 h-full" >
 
-Gonna have the map here when am not lazy
+<DonationWidget />
 
                </div>
                <div class="w-full lg:w-1/2 bg-white min-h-full py-12 px-8 sm:px-40">
